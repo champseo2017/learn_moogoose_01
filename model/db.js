08335838d2bee5f43b0637a999ec0b3146b30aef
champseo2017 <boomgt4444@gmail.com>
2022-03-29 09:37:14 +0700

    Custom SchemaTypes

--- model/db.js
+++ model/db.js
@@ -26,87 +26,15 @@ process.on("SIGINT", () => {
 });
 
 
-// Data types allowed in schemas
-/* 
+// Custom SchemaTypes
+/*
 
-- Eight types (SchemaTypes)
-1. String
-2. Number
-3. Date
-4. Boolean
-5. Buffer
-6. ObjectId
-7. Mixed
-8. Array
+If your data requires a different datatype
+Mongoose offers the option of extending it with custom SchemaTypes. The extension 
+method is managed using Mongoose plugins
+long, double, RegEx, and even email.
 
-- String
-This SchemaType stores a string value, UTF-8 encoded.
 
-- Number
-This SchemaType stores a number value (Mongoose does not  natively support long and double datatypes) // Mongoose can be extended using plugins to support these other  types
-
-- Date
-This SchemaType holds a date and time object, typically returned from MongoDB  as an ISODate object ( ISODate("2013-04-03T12:56:26.009Z") )
-
-Buffer
-This SchemaType is primarily used for storing binary 
-(Example)
-- images stored in MongoDB.
-
-ObjectId
-- for example, a foreign key for referencing
-- Rather than just specifying the type of ObjectId you 
-need to specify the fully qualified version Schema.Types.ObjectId. For example:
-projectSchema.add({
- owner: mongoose.Schema.Types.ObjectId
-});
-
-Mixed
-A mixed data object can contain any type of data. It can be declared either by 
-setting an empty object, or by using the fully qualified Schema.Types.Mixed. 
-These following two commands will do the same thing:
-
-vardjSchema= new mongoose.Schema({
- mixedUp: {} 
-});
-
-vardjSchema= new mongoose.Schema({
- mixedUp: Schema.Types.Mixed
-});
-
-mixed type cannot be automatically detected by Mongoose, so it doesn't know that it 
-needs to save them.
-
-Tracking changes to Mixed type
-
-dj.mixedUp = { valueone: "a new value" };
-dj.markModified('mixedUp');
-dj.save();
-
-Array
-var userSchema = new mongoose.Schema({
- name: String,
- emailAddresses: [String]
-});
-
-Second, the array datatype can be used to store a collection of subdocuments using 
-nested schemas.
-
-var userSchema = new mongoose.Schema({
- name: String,
- emailAddresses: [emailSchema]
-});
-
-Warning – array defined as mixed type
-. If you declare an empty array it will be treated as mixed type, 
-meaning that Mongoose will not be able to automatically detect any changes made to 
-the data.
-var emailSchema = new mongoose.Schema({
- addresses: []
-});
-var emailSchema = new mongoose.Schema({
- addresses: Array
-});
 
 
 
