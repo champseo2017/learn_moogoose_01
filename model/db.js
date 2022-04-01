7f11d00cbc762735ec3a8dce3d6e5b76b4c3066b
champseo2017 <boomgt4444@gmail.com>
2022-04-01 08:58:20 +0700

    Defining the Project schema

--- model/db.js
+++ model/db.js
@@ -26,21 +26,28 @@ process.on("SIGINT", () => {
 });
 
 
-// Writing a schema
+// Defining the Project schema
 /*
-declare a variable to hold the schema
-for example, user or project and adding Schema to the end 
-of it.
-var userSchema = new mongoose.Schema({ });
+Project schema.
 
-Only allowing unique entries
-For example, if we want to ensure that there is only ever one user per e-mail address, 
-we can specify that the email field should be unique.
+• projectName: A string containing the name of the project.
+• createdOn: The date when the document was first created and saved. This 
+option is set to automatically save the current date and time..
+• modifiedOn: The date and time when the document was last changed.
+• createdBy: A string that will for now contain the unique ID of the user who 
+created the project.
+• tasks: A string to hold task information.
 
-email: {type: String, unique:true}
+varprojectSchema = new mongoose.Schema({
+ projectName: String,
+ createdOn: Date,
+ modifiedOn: { type: Date, default: Date.now },
+ createdBy: String,
+ tasks: String
+});
 
-MongoDB will check to see if 
-the e-mail value already exists in another document. If it finds it, MongoDB (not 
-Mongoose) will return an E11000 error.
+
+
+
 
 */
